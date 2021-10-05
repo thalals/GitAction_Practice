d8d460119adc65df04fd205384ed7eb5ddbf91a7
thalals <thalsal@naver.com>
2021-10-05 17:27:08 +0900

    gitaciton prac

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ mysql.init_app(application)
 
 @application.route('/')
 def main():
-    return "핵심 쏙쏙 AWS"
+    return "핵심 쏙Tjr쏙 AWS"
 
 @application.route('/fileupload', methods=['POST'])
 def file_upload():
